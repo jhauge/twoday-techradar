5f1a12f025e97252ca582da8bbc808b54182d04e
Jesper Hauge <jesper@iqit.dk>
2025-02-06 16:43:03 +0100

    fix: moved projectid to .env file

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,9 +1,12 @@
 import {defineCliConfig} from 'sanity/cli'
 
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID
+if (!projectId) { throw new Error('The project ID is required') }
+
 export default defineCliConfig({
   api: {
-    projectId: '',
-    dataset: ''
+    projectId,
+    dataset: 'production'
   },
   /**
    * Enable auto-updates for studios.
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,13 +2,15 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
-import 'dotenv/config'
+
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID
+if (!projectId) { throw new Error('The project ID is required') }
 
 export default defineConfig({
   name: 'default',
   title: 'TechRadar',
 
-  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
+  projectId,
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
